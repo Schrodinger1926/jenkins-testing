97d4ce7cdafc9ffd2b95a07fa1a139081d39d647
sidharth sindhra <sidharth.sindhra@gmail.com>
2018-08-10 15:56:46 +0530

    final restruct

---
+++ cpp_testing/square.cpp
@@ -0,0 +1,5 @@
+#include "square.h"
+
+int square(int a){
+    return a*a;
+}
---
+++ cpp_testing/test_square.cpp
@@ -0,0 +1,17 @@
+#include <gtest/gtest.h>
+
+#include "square.h"
+
+TEST(squareTest, testingZeroCase){
+    EXPECT_EQ(square(0), 0);
+}
+
+TEST(squareTest, testingPositiveCase){
+    EXPECT_EQ(square(2), 4);
+}
+
+int main(int argc, char* argv[]){
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
